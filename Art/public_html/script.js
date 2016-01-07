cec2bf7aeb23fa3186d05c8d54271d49bb0b0b8a
joellemachaalany <joellemachaalany@gmail.com>
2016-01-07 22:08:20 +0200

    photography

--- Art/public_html/script.js
+++ Art/public_html/script.js
@@ -87,3 +87,93 @@ function reset() {
     document.getElementById("message").reset();
 }
 
+var picture;
+function changePicture() {
+    picture = window.setInterval(getPicture, 3000);
+}
+function getPicture() {
+    var pic;
+    var rnd = Math.random();
+    if (rnd < 0.34) {
+        pic = "images/car.jpg";
+    } else if (rnd < 0.67) {
+        pic = "images/cat.jpg";
+    } else {
+        pic = "images/balloon.jpg";
+    }
+    document.getElementById("pictures1").src = pic;
+}
+
+var picture2;
+function changePicture2() {
+    picture2 = window.setInterval(getPicture2, 3000);
+}
+function getPicture2() {
+    var pic2;
+    var rnd = Math.random();
+    if (rnd < 0.34) {
+        pic2 = "images/eiffel tower.jpg";
+    } else if (rnd < 0.67) {
+        pic2 = "images/fisheyes.jpg";
+    } else {
+        pic2 = "images/handstand.jpg";
+    }
+    document.getElementById("pictures2").src = pic2;
+}
+
+var picture3;
+function changePicture3() {
+    picture3 = window.setInterval(getPicture3, 3000);
+}
+function getPicture3() {
+    var pic3;
+    var rnd = Math.random();
+    if (rnd < 0.34) {
+        pic3 = "images/lastdrop.jpg";
+    } else if (rnd < 0.67) {
+        pic3 = "images/photography1.jpg";
+    } else {
+        pic3 = "images/sunset.jpg";
+    }
+    document.getElementById("pictures3").src = pic3;
+}
+
+var picture4;
+function changePicture4() {
+    picture4 = window.setInterval(getPicture4, 3000);
+}
+function getPicture4() {
+    var pic4;
+    var rnd = Math.random();
+    if (rnd < 0.34) {
+        pic4 = "images/upsidedown.jpg";
+    } else if (rnd < 0.67) {
+        pic4 = "images/tiger.jpg";
+    } else {
+        pic4 = "images/train.jpg";
+    }
+    document.getElementById("pictures4").src = pic4;
+}
+
+var picture5;
+function changePicture5() {
+    picture5 = window.setInterval(getPicture5, 3000);
+}
+function getPicture5() {
+    var pic5;
+    var rnd = Math.random();
+    if (rnd < 0.34) {
+        pic5 = "images/wave.jpg";
+    } else if (rnd < 0.67) {
+        pic5 = "images/tree.jpg";
+    } else {
+        pic5 = "images/wine.jpg";
+    }
+    document.getElementById("pictures5").src = pic5;
+}
+
+(function onChange() {
+    $('a').attr('href', 'images/car.jpg');
+    $('a').attr('href', 'images/cat.jpg');
+    $('a').attr('href', 'images/balloon.jpg');
+});
